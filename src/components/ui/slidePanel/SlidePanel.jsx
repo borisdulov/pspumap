277a5ad64eee2e0c738acef7c4ca79774ada967e
Boris Dulov <borisdulov@MacBook-Air-Boris.local>
2024-05-30 18:17:21 +0500

    new pano lib

--- src/components/ui/slidePanel/SlidePanel.jsx
+++ src/components/ui/slidePanel/SlidePanel.jsx
@@ -1,14 +1,18 @@
-import { Pannellum } from "pannellum-react";
-import React, { useState } from 'react'
+import React from 'react'
+import { ReactPhotoSphereViewer } from 'react-photo-sphere-viewer'
 
 import './slidePanel.css'
-import MyPanorama from "./myPanorama/MyPanorama";
+// import MyPanorama from "./myPanorama/MyPanorama";
 
 export default function SlidePanel({ isPanelOpen, panoName, setIsPanelOpen }) {
   return (
     <>
       <div className={`panel ${isPanelOpen ? 'open' : 'closed'}`}>
-        <MyPanorama panoName={panoName}/>
+        <ReactPhotoSphereViewer
+          src="left.png"
+          height={'100%'}
+          width={"100%"}
+        />
       </div>
       <button
         className={`panel-button ${isPanelOpen ? 'active' : 'hidden'}`}
--- src/components/ui/slidePanel/myPanorama/MyPanorama.jsx
+++ src/components/ui/slidePanel/myPanorama/MyPanorama.jsx
@@ -1,22 +1,46 @@
-import { Pannellum } from 'pannellum-react';
+// import { Pannellum } from 'pannellum-react';
 
-const MyPanorama = ({ panoName }) => {
-  return (
-    <div className='pano'>
-      <Pannellum
-        image={'/' + panoName + '.png'}
-        hfov={170 * (window.innerWidth / window.innerHeight / 2)}
-        autoLoad={true}
-        mouseZoom={true}
-        height="100%"
-        minHfov={10}
-        showZoomCtrl={false}
-        compass={false}
-        showFullscreenCtrl={false}
-        autoRotate={-2}>
-      </Pannellum>
-    </div>
-  );
-}
+// const MyPanorama = ({ panoName }) => {
+//   return (
+//     <div className='pano'>
+//       <Pannellum
+//         image={'/' + panoName + '.png'}
+//         hfov={170 * (window.innerWidth / window.innerHeight / 2)}
+//         autoLoad={true}
+//         mouseZoom={true}
+//         height="100%"
+//         minHfov={10}
+//         showZoomCtrl={false}
+//         compass={false}
+//         showFullscreenCtrl={false}
+//         autoRotate={-2}>
+//       </Pannellum>
+//     </div>
+//   );
+// }
 
-export default MyPanorama;
+// export default MyPanorama;
+
+// import * as Pannellum from 'pannellum';
+
+// const MyPanorama = ({ panoName }) => {
+//   const viewer = Pannellum.Viewer(document.getElementById('pannellum-container'));
+
+//   viewer.setPanorama('/' + panoName + '.png');
+//   viewer.setHfov(150 * (window.innerWidth / window.innerHeight / 2));
+//   viewer.setAutoLoad(true);
+//   viewer.setMouseZoom(true);
+//   viewer.setMinHfov(10);
+//   viewer.setShowZoomCtrl(false);
+//   viewer.setCompass(false);
+//   viewer.setShowFullscreenCtrl(false);
+//   viewer.setAutoRotate(-2);
+
+//   return (
+//     <div className='pano'>
+//       <div id='pannellum-container' style={{ height: '100%' }} />
+//     </div>
+//   );
+// }
+
+// export default MyPanorama;
